3fd0a0c60f2cec768c66a166aea5d7b641ccfbb1
sadhchanGitHub <sadhvi.chandragiri@outlook.com>
2025-01-10 21:04:04 +0100

    push the projects as of now 2025.01.10 v2

--- cas_deepL_project/src/06_b_execute_cnn_eurosat_binary_classify_evaluate.py
+++ cas_deepL_project/src/06_b_execute_cnn_eurosat_binary_classify_evaluate.py
@@ -2,12 +2,11 @@ import os
 import numpy as np
 import tensorflow as tf
 from sklearn.metrics import classification_report, confusion_matrix
-
 import logging
 import time
-import os
 from datetime import datetime
-
+import sys
+from tensorflow.keras.models import load_model
 import matplotlib
 matplotlib.use('Agg')  # Use 'Agg' backend for non-interactive plotting
 import matplotlib.pyplot as plt
@@ -15,26 +14,23 @@ import seaborn as sns
 
 timestamp = int(time.time())
 
+def chk_gpu_config():
+    gpus = tf.config.list_physical_devices('GPU')
+    if gpus:
+        try:
+            tf.config.set_visible_devices(gpus[0], 'GPU')
+            tf.config.experimental.set_memory_growth(gpus[0], True)
+            print("GPU configured successfully.")
+        except RuntimeError as e:
+            print(f"Error configuring GPU: {e}")
 
-
-def load_data(data_dir, categories, split):
-    """
-    Load test data from .npy files for all categories.
-    """
-    test_images = []
-    test_labels = []
-
-    test_images = np.concatenate(test_images, axis=0)
-    test_labels = np.concatenate(test_labels, axis=0)
-
-    test_images = np.load(os.path.join(training_dir_remapped, f"binary_{split}_images.npy"))
-    test_labels = np.load(os.path.join(training_dir_remapped, f"binary_{split}_labels.npy"))
-    logging.info(f"Test images shape: {test_images.shape}")
-    logging.info(f"Test labels shape: {test_labels.shape} \n")
-    
+def load_data(data_dir, split):
+    test_images = np.load(os.path.join(data_dir, f"binary_{split}_images.npy"))
+    test_labels = np.load(os.path.join(data_dir, f"binary_{split}_labels.npy"))
+    logging.info(f"{split.capitalize()} images shape: {test_images.shape}")
+    logging.info(f"{split.capitalize()} labels shape: {test_labels.shape}")
     return test_images, test_labels
-
-def evaluate_model(model_path, data_dir, categories, input_shape):
+def evaluate_model(model_path, model_name_trim, data_dir, input_shape):
     """
     Evaluate a trained model on the test set.
     """
@@ -47,109 +43,78 @@ def evaluate_model(model_path, data_dir, categories, input_shape):
     test_images, test_labels = load_data(data_dir, split="test")
     logging.info(f"Test data shape: {test_images.shape}, Test labels shape: {test_labels.shape}")
     
-    # Normalize the images (if not already normalized)
-    # test_images = test_images / 255.0  # Ensure pixel values are in the range [0, 1]
-    
     # Evaluate the model
-    loss, accuracy = model.evaluate(test_images, test_labels, verbose=1)
-    print(f"Test Loss: {loss:.4f}, Test Accuracy: {accuracy:.4f}\n")
-    logging.info(f"Test Loss: {loss:.4f}, Test Accuracy: {accuracy:.4f}\n")
+    results = model.evaluate(test_images, test_labels, verbose=1)
+    
+    # Print all metrics
+    metrics = model.metrics_names
+    for metric_name, result in zip(metrics, results):
+        print(f"{metric_name}: {result:.4f}")
+        logging.info(f"{metric_name}: {result:.4f}")
     
     # Predict on test data
     predictions = model.predict(test_images)
-    predicted_classes = np.argmax(predictions, axis=1)
-    true_classes = np.argmax(test_labels, axis=1)
+    predicted_classes = (predictions > 0.5).astype("int32").reshape(-1)
+    true_classes = test_labels.reshape(-1)
+
+    # we now have only two categories
+    class_names = ['Residential', 'HerbaceousVegetation']
     
     # Generate classification report
     print("Classification Report:\n")
-    clf_report = classification_report(true_classes, predicted_classes, target_names=categories)
-    logging.info(f"Classification Report'")
+    clf_report = classification_report(true_classes, predicted_classes, target_names=class_names)
+    logging.info("Classification Report")
     logging.info(clf_report)
     
-    # Generate confusion matrix
+
+    
+    # Clear previous figures
+    plt.clf()
+    plt.cla()
+    plt.close('all')
     conf_matrix = confusion_matrix(true_classes, predicted_classes)
-    # Plot confusion matrix
-    plt.figure(figsize=(10, 8))
-    sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues', xticklabels=categories, yticklabels=categories)
-    plt.title("Confusion Matrix")
+    cfmatrix_fig = plt.figure(figsize=(10, 8))  # Adjust size if needed
+    sns.heatmap(conf_matrix, annot=True, fmt="d", cmap="Blues", xticklabels=class_names, yticklabels=class_names)
     plt.xlabel("Predicted")
-    plt.ylabel("True")
-    plt.savefig(f"../outputs/06_b_{model_name}_evaluation_confusion_matrix_{timestamp}.png")
-    logging.info(f"Confusion matrix saved as '06_b_{model_name}_evaluation_confusion_matrix_{timestamp}.png'. \n")
-
-def sanity_check_testdata(training_dir_remapped):
-    test_images = np.load(os.path.join(training_dir_remapped, "binary_test_images.npy"))
-    test_labels = np.load(os.path.join(training_dir_remapped, "binary_test_labels.npy"))
-    logging.info(f"train_images shape: {test_images.shape}")
-    logging.info(f"train_labels shape: {test_labels.shape}")
-    logging.info(f"Sample labels: {test_labels[:5]}")
-
+    plt.ylabel("Actual")
+    plt.title("Confusion Matrix")
+    cfmatrix_fig.savefig(f'../outputs/06_b_{model_name_trim}_evaluation_confusion_matrix_{timestamp}.png')
+    plt.close(cfmatrix_fig)
+    logging.info(f"Confusion matrix saved as '06_b_{model_name_trim}_evaluation_confusion_matrix_{timestamp}.png'. \n")
 
 
 def main():
     try:
-        
-        # Logging configuration
         log_dir = "../logs"
         os.makedirs(log_dir, exist_ok=True)
         log_file = os.path.join(log_dir, f"06_b_execute_cnn_eurosat_binary_classify_evaluate_{timestamp}.log")
-        
+
         logging.basicConfig(
             level=logging.INFO,
             format="%(asctime)s - %(message)s",
             handlers=[logging.FileHandler(log_file), logging.StreamHandler()],
         )
-        
-        logging.info("\n")
-        logging.info("called via 06_b_execute_cnn_eurosat_binary_classify_evaluate.py...\n")
-        logging.info(" Script Started ...\n")
-        logging.info("This script will evaluate the cnn model passed via args along with image_size \n")
 
-        # Check if image size is passed as an argument
+        logging.info("Script Started...")
+
         if len(sys.argv) != 3:
-            print("Usage: python 06_b_execute_cnn_eurosat_binary_classify_evaluate.py <image_size>")
+            print("Usage: python 06_b_execute_cnn_eurosat_binary_classify_evaluate.py <image_size> <model_name>")
             sys.exit(1)
-    
-        # Get image size from the command-line argument
+
         image_size = int(sys.argv[1])
-        logging.info(f"image_size is {image_size} \n ")
-
-
-        # Get the model path from the command-line argument
         model_name = sys.argv[2]
-        # Load the model using the provided path
-        model = load_model(f"../models/{model_name}")
-        logging.info(f"model_name is {model_name} \n ")
+        model_path = f"../models/{model_name}"
 
-        # call gpu usage
+        model_name_trim = model_name.replace(".keras", "")
+
         chk_gpu_config()
 
-        training_dir_remapped = f"../data/training_data_remapped_binary_{image_size}"
-        logging.info(f"training_dir_remapped is {training_dir_remapped} \n ")
-        batch_size = 32
-        logging.info(f"batch_size is {batch_size} \n ")
-
-        # Sanity check
-        sanity_check_testdata(training_dir_remapped)
-
-        #derive input_size from image_size
-        input_shape = (image_size, image_size, 3)  # Shape of input images
-        num_classes = 2  # Number of categories
-        logging.info(f"batch_size is {input_shape} and num_classes is {num_classes} \n ")
-        
-        # Run evaluation
-        evaluate_model(model, training_dir_remapped, input_shape)
+        data_dir = f"../data/training_data_remapped_binary_{image_size}"
+        evaluate_model(model_path, model_name_trim, data_dir, (image_size, image_size, 3))
 
     except Exception as e:
-        logging.error(f"Error during script : {e}")
+        logging.error(f"Error during script execution: {e}")
         raise
 
-
-    logging.info(f"Script Ended at {time.strftime('%Y-%m-%d %H:%M:%S')}\n")
-
 if __name__ == "__main__":
-    try:
-        main()
-    except Exception as e:
-        logging.critical(f"Script failed: {e}")
-    
+    main()
